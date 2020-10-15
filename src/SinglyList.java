a6f044fce3e78df4ef645f1c95fa16d15dda7e7b
q15751261559 <3328585084@qq.com>
2020-10-16 01:38:27 +0800

    链表合并

--- src/SinglyList.java
+++ src/SinglyList.java
@@ -1,7 +1,7 @@
 import com.sun.deploy.security.SelectableSecurityManager;
 import java.util.ArrayList;
 
-public class SinglyList<T> extends Object {
+public class SinglyList<T extends Comparable> extends Object {
 	public Node<T> head;
 	public SinglyList()
 	{
@@ -142,19 +142,25 @@ public class SinglyList<T> extends Object {
 		}
 		this.head.next=front;
 	}
-	public boolean equals(Object anObject)
-	{
-		String str=this.toString();
-		String str2=anObject.toString();
-		return str.equalsIgnoreCase(str2);
-	}
-	public int compareTo(Object anObject)
-	{
-		String str=this.toString();
-		String str2=anObject.toString();
-		if (str.compareTo())
-	}
-	public Node<T> Merge(SinglyList list2)
+//	public boolean equals(Object anObject)  //重写equals
+//	{
+//		String str=this.toString();
+//		String str2=anObject.toString();
+//		System.out.println(this.toString());
+//		return str.equalsIgnoreCase(str2);
+//	}
+//	public int CompareTo(Object anObject)   //添加compareTo方法
+//	{
+//		String str=this.toString();
+//		String str2=anObject.toString();
+//		if (str.compareTo(str2)<0)
+//			return -1;
+//		else if (str.compareTo(str2)>0)
+//			return 1;
+//		else
+//			return 0;
+//	}
+	public Node<T> Merge1(SinglyList list2)  //不重复
 	{
 		Node<T> p,q,t,s;
 		p=this.head.next;
@@ -171,7 +177,7 @@ public class SinglyList<T> extends Object {
 		}
 		while (p!=null && q!=null)
 		{
-			if (p.data.equals(q.data)) {
+			if (p.data.equals(q.data)){
 				p = p.next;
 				q = q.next;
 				t = t.next;
@@ -181,13 +187,89 @@ public class SinglyList<T> extends Object {
 				} else {
 					s = s.next;
 				}
-			}else if (p.data.)
+			}else if (p.data.compareTo(q.data)>0)
+			{
+				q.next=t.next;
+				t.next=q;
+				t=t.next;
+				if (s==null)
+				{
+					break;
+				}
+				q=s;
+				s=s.next;
+			}else if (p.data.compareTo(q.data)<0)
+			{
+//				System.out.println(p.data.compareTo(q.data));
+				p=p.next;
+				t=t.next;
+			}
+			if (p==null)
+			{
+				t.next=q;
+			}
 		}
+		return this.head;
+	}
+
+	public Node<T> Merge2(SinglyList list2)  //重复
+	{
+		Node<T> p,q,t,s,z;
+		p=this.head.next;
+		t=this.head;
+		q=list2.head.next;
+		s=q.next;
+		if (p==null)
+		{
+			return (list2.head);
+		}
+		if (q==null)
+		{
+			return (this.head);
+		}
+		while (p!=null && q!=null)
+		{
+			if (p.data.equals(q.data)){
+				p = p.next;
+				z=q;
+				q = q.next;
+				z.next=t.next;
+				t.next=z;
+				t = z.next;
+
+				if (s == null) {
+					break;
+				} else {
+					s = s.next;
+				}
+			}else if (p.data.compareTo(q.data)>0)
+			{
+				q.next=t.next;
+				t.next=q;
+				t=t.next;
+				if (s==null)
+				{
+					break;
+				}
+				q=s;
+				s=s.next;
+			}else if (p.data.compareTo(q.data)<0)
+			{
+//				System.out.println(p.data.compareTo(q.data));
+				p=p.next;
+				t=t.next;
+			}
+			if (p==null)
+			{
+				t.next=q;
+			}
+		}
+		return this.head;
 	}
 	public static void main(String[] args) {
-		String[] value = { "1","2","3","4"};
-		SinglyList<String> seq1 = new SinglyList<String>(value);
-		System.out.println("原数组"+seq1.toString());
+//		String[] value = { "1","2","3","4"};
+//		SinglyList<String> seq1 = new SinglyList<String>(value);
+//		System.out.println("原数组"+seq1.toString());
 //		seq1.set(3,"jiahui");
 //		System.out.println("数组"+seq1.toString());
 //		System.out.println(seq1.search("a"));
@@ -196,9 +278,19 @@ public class SinglyList<T> extends Object {
 //		System.out.println(seq1.remove("jiahui"));
 //		System.out.println("数组"+seq1.toString());
 //		System.out.println(seq1.size());
-		seq1.Reverse1();
-		System.out.println("数组"+seq1.toString());
-		seq1.Reverse2();
-		System.out.println("数组"+seq1.toString());
+//		seq1.Reverse1();
+//		System.out.println("数组"+seq1.toString());
+//		seq1.Reverse2();
+//		System.out.println("数组"+seq1.toString());
+		Integer[] number1={1,2,3,4517};
+		Integer[] number2={8,11,328,4517};
+		SinglyList<Integer> num1=new SinglyList<>(number1);
+		SinglyList<Integer> num2=new SinglyList<>(number2);
+		System.out.println("一数组"+num1.toString());
+		System.out.println("二数组"+num2.toString());
+//		num1.Merge1(num2);
+//		System.out.println("非重复合并链表"+num1.toString());
+		num1.Merge2(num2);
+		System.out.println("重复合并链表"+num1.toString());
 	}
 }
